463f3e0f8744d42fad2bf7b799fedbd3d220ff41
Mark Coggeshall <mark.coggeshall@gmail.com>
2017-07-10 10:01:43 -0400

    Initial commit of files created by cookiecutter.

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use pyncrypt in a project::
+
+    import pyncrypt
---
+++ pyncrypt/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pyncrypt."""
+
+__author__ = """Mark Coggeshall"""
+__email__ = 'mark.coggeshall@gmail.com'
+__version__ = '0.1.0'
