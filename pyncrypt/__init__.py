508721012ebc56be66363f3df9c6e2e93bd768ca
Mark Coggeshall <mark.coggeshall@gmail.com>
2017-07-10 17:50:33 -0400

    Added import statement to __init__ file.

--- pyncrypt/__init__.py
+++ pyncrypt/__init__.py
@@ -2,6 +2,8 @@
 
 """Top-level package for pyncrypt."""
 
+from pyncrypt.pyncrypt import KeyStore
+
 __author__ = """Mark Coggeshall"""
 __email__ = 'mark.coggeshall@gmail.com'
 __version__ = '0.1.0'
